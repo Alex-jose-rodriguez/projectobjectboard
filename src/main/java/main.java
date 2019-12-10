9b1393f02e33249b2bb51cb575a8902b462b056e
alex.rodriguez <rodriquez.alex@gmail.com>
2019-12-10 10:04:56 -0500

    Configurado JPA con Hibernate multi-modular

---
+++ src/main/java/main.java
@@ -0,0 +1,8 @@
+import com.lacroix.lacroixnet.controller.LanguageController;
+
+public class main {
+    public static void main(String [] args){
+        LanguageController languageController = new LanguageController();
+        languageController.sampleDetailLanguage();
+    }
+}
