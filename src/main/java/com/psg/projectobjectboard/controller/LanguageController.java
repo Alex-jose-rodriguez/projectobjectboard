be0f7a8c0a329a98f935a05136ef0095e6d2fe6f
alex <rodriquez.alex@gmail.com>
2020-02-05 10:51:15 -0500

    Proyecto tablero de objetos

---
+++ src/main/java/com/psg/projectobjectboard/controller/LanguageController.java
@@ -0,0 +1,19 @@
+package com.psg.projectobjectboard.controller;
+import com.psg.projectobjectboard.nucleus.datatransferobject.LanguageDto;
+import com.psg.projectobjectboard.nucleus.service.LanguageService;
+
+import java.util.Scanner;
+
+public class LanguageController {
+    private LanguageService languageService;
+
+    public LanguageController(){this.languageService = new LanguageService();}
+
+    public void sampleDetailLanguage(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Quel est l'identifiant de la langue dont vous voulez afficher les informations ?");
+        long identifier=scanner.nextLong();
+        LanguageDto languageDto= languageService.getLanguage(identifier);
+        System.out.println("La langue sélectionné s'applle "+ languageDto.getName());
+    }
+}
